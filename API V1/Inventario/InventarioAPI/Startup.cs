1dd311507573e6cf1a8a1f523d6095e0c0209457
csegura <kill4n@users.noreply.github.com>
2021-10-17 12:24:53 -0500

    Fix connection
    Present swagger and disable https

--- API V1/Inventario/InventarioAPI/Startup.cs
+++ API V1/Inventario/InventarioAPI/Startup.cs
@@ -44,14 +44,14 @@ namespace InventarioAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "InventarioAPI v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
